4d22a4e250eb51da3df52715cef2b93ea60ea0a5
developer111222 <anishprajapati765@gmail.com>
2025-03-24 11:11:53 +0530

    first commit

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -3,6 +3,9 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import { MdLocalPhone } from "react-icons/md";
+import { BsEnvelopePlusFill } from "react-icons/bs";
+import { LuDownload } from "react-icons/lu";
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -16,6 +19,31 @@ export default function Header() {
 
   return (
     <header className="bg-black shadow-md sticky top-0 z-50">
+  <div className="container mx-auto px-4 sm:px-6 lg:px-8 hidden sm:block">
+
+<div className="flex justify-between p-5 items-center">
+  <div className="flex-1">
+<div className="flex justify-start gap-20">
+<div className="flex items-center gap-1 ">
+<MdLocalPhone className="" color="white" />
+<p className="text-white">+91-8950867889</p>
+</div>
+<div className="flex items-center gap-2">
+<BsEnvelopePlusFill color="white"/>
+<p className="text-white">accord_cables@yahoo.co.in</p>
+</div>
+</div>
+  </div>
+  <div className="flex-1">
+<div className="flex justify-end  ">
+<a href="/Accord-Cables-Brochure.pdf" download className="">
+  <button className="text-white flex gap-3 items-center border-2 border-white p-2 rounded-full cursor-pointer"><LuDownload color="white" /> Download</button>
+  </a>
+</div>
+  </div>
+</div>
+    </div>
+
       <nav className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
