9006f5354e655cda0577c73cd491f0ff6effa3e5
developer111222 <anishprajapati765@gmail.com>
2025-03-28 10:42:09 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,27 @@ import { Review } from "./Home/Review";
 import ProductLink from './Home/ProductLink';
 import Page from './product/page';
 import Certificate from './Home/Certificate';
+import HouseWire from './Home/HouseWire';
+import MulticoreFlexible from './Home/MulticoreFlexible';
+import SubmersibleCable from './Home/SubmersibleCable';
+import ServiceWire from './Home/ServiceWire';
 
 
 
 export default function Home() {
   return (
    <>
-<GoogleGeminiEffectDemo/>
+{/* <GoogleGeminiEffectDemo/> */}
+<HouseWire/>
+<MulticoreFlexible/>
+<SubmersibleCable/>
+<ServiceWire/>
 <CardHoverEffectDemo/>
 <Certificate />
-<Page/>
+
 {/* <ProductLink/> */}
-<WireAndCable/>
-<Review/>
+{/* <WireAndCable/> */}
+{/* <Review/> */}
 
    </>
   );
